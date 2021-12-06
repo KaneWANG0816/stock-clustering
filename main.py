99b1ab7c8bd1b6dd77f5b057e19f304c59d476f3
KaneWANG0816 <kanewang0816@gmail.com>
2021-12-06 23:15:42 +0800

    init

---
+++ main.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+
+# Problem1: which features are suitable for stock price prediction?
+# Problem2: which company are similar(correlation), for diversifying portfolio?
+# Problem3: which
+
+print("yes")
